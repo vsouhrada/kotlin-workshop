1696738ef3feda2c926ba7a2b21b5e9baed773f3
vaclav.souhrada <vaclav.souhrada@eman.cz>
2017-12-04 14:17:22 +0100

    Added dokka

--- src/main/kotlin/com/github/vsouhrada/kotlin/workshop/Main.kt
+++ src/main/kotlin/com/github/vsouhrada/kotlin/workshop/Main.kt
@@ -1,1 +1,13 @@
 package com.github.vsouhrada.kotlin.workshop
+
+
+
+/**
+ * This is demo class
+ * @author vsouhrada
+ * @constructor This is primary constructor
+ * @param[name] this is param name
+ */
+class Demo(name: String) {
+
+}
